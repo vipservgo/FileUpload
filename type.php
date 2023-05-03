ba88578599f62fe1247c2ecab706d06591a9b5c4
RubY <92526630+vipservgo@users.noreply.github.com>
2023-05-03 21:39:56 +0400

    Add files via upload

---
+++ type.php
@@ -0,0 +1,141 @@
+<?php
+
+	function folderSize ($dir)
+	{
+	    $size = 0;
+	    foreach (glob(rtrim($dir, '/').'/*', GLOB_NOSORT) as $each) {
+	        $size += is_file($each) ? filesize($each) : folderSize($each);
+	    }
+	    return $size;
+	}
+
+	$capFile = fopen("src/set/disableDel.dat", "r") or die("Unable to open file!");
+    $delStat = fread($capFile,filesize("src/set/disableDel.dat"));
+    fclose($capFile);
+
+	$typ = $_GET['typ'];
+	if($typ==1){
+		$dir = "uploads/img/";
+		$pTitle = "Images";
+	}
+	elseif($typ==2){
+		$dir = "uploads/vid/";
+		$pTitle = "Videos";
+	}
+	elseif($typ==3){
+		$dir = "uploads/aud/";
+		$pTitle = "Audio";
+	}
+	elseif($typ==4){
+		$dir = "uploads/oth/";
+		$pTitle = "Others";
+	}
+	else{
+
+	}
+	$files = scandir($dir);
+	$fileCount = count($files);
+	$dirSize = (folderSize($dir)/1024)/1024;
+	if($dirSize<1024){
+		$precision = 2;
+		$dirSize = substr(number_format($dirSize, $precision+1, '.', ''), 0, -1);
+		$dirSize= $dirSize." MB";
+	}
+	else{
+		$precision = 2;
+		$dirSize = $dirSize/1024;
+		$dirSize = substr(number_format($dirSize, $precision+1, '.', ''), 0, -1);
+		$dirSize= $dirSize." GB";
+	}
+	echo '
+	<!DOCTYPE html>
+	<html>
+	<meta name="theme-color" content="#111">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<head>
+	  <title>'.$pTitle.'</title>
+	  <link rel="stylesheet" href="src/css/type.css">
+	  <script type="text/javascript" src="src/script/script.js"></script>
+	</head>
+	<body>
+	<body id="body">
+		<div class="topbar shadowDeep">
+			<a href="index.php" class="whiteLink">Back</a>
+			<div class="rightPanel">'.$dirSize.'</div>
+		</div>
+		<div class="main">
+	';
+	if($fileCount>4){
+		for($i=2;$i<$fileCount;$i++){
+			if($files[$i]=="index.php")
+				continue;
+			$fileLoc = $dir.$files[$i];
+				if($files[$i]!=".notempty"){
+					if($typ==4){
+						echo "<a href='download.php?i=".$fileLoc."'>";
+					}
+					else{
+						echo "<a href='$fileLoc'>";
+					}
+					echo"
+						<div class='fileName shadowLight'>
+						";
+							//The thumbnail
+							if($typ==1){
+								echo "
+									<div class='thumbnailHolder'><img src='$fileLoc' class='thumbnail'></div>
+								";
+								$loc = "uploads/img/";
+							}
+							if($typ==2){
+								echo "
+									<div class='thumbnailHolder lessPadding'><img src='src/img/vid.png' class='thumbnail'></div>
+								";
+								$loc = "uploads/vid/";
+							}
+							if($typ==3){
+								echo "
+									<div class='thumbnailHolder lessPadding'><img src='src/img/aud.png' class='thumbnail'></div>
+								";
+								$loc = "uploads/aud/";
+							}
+							if($typ==4){
+								echo "
+									<div class='thumbnailHolder lessPadding'><img src='src/img/oth.png' class='thumbnail'></div>
+								";
+								$loc = "uploads/oth/";
+							}
+
+						echo"
+							<div class='nameTxt'>
+								";
+								$fileAddr = $loc.$files[$i];
+								$files[$i] = substr($files[$i], 0, 32);
+								echo "
+								$files[$i]
+							</div>
+							";
+							if($delStat==0){
+								echo "
+									<a href='del.php?id=$fileAddr&t=$typ'>
+										<img src='src/img/delete.png' title='Delete File' class='delBtn'>
+									</a>
+								";
+							}
+							echo "
+						</div>
+					<a/>";
+				}
+		}
+	}
+	else{
+		//The folder is empty
+		echo "<div style='font-family:arial;color:#555;font-size:18px;'>Folder is empty</div>";
+	}
+	echo "
+			</div>
+		</body>
+		</html>
+
+	";
+?>
